3d1a4dc210f019bf7129f30e1f554ccfb8921598
Arash Zakeresfahani <azakeres@iu.edu>
2019-10-31 19:51:36 -0400

    f_hat has been optimized

--- llr_functions.R
+++ llr_functions.R
@@ -24,9 +24,7 @@ compute_f_hat = function(z, x, y, omega) {
   Wz = diag(make_weight_matrix(z, x, omega))
   X = make_predictor_matrix(x)
   n = nrow(X)
-  A = t(sapply(1:n, function(i){
-    Wz[i]*X[i,]
-  }))
+  A = sweep(X, 1, Wz, "*") 
   f_hat = c(1,z) %*% solve(t(X)%*% A) %*% t(X) %*% (Wz*y)
   #f_hat = c(1, z) %*% solve(t(X) %*% Wz %*% X) %*% t(X) %*% Wz %*% y
   return(f_hat)
